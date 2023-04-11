54b407965f529c483ce663f3978931cb8e97057d
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-04-11 07:53:46 +0700

    refactor and update
    updated links in data.js, refactored and cleaned up the code throughout, added an image to index.html and styled it, removed unnecessary files

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 export const projectsArray = [
     {
-        id: "case-study-link",
+        id: "case-study-cyclistic",
         href: "https://rpubs.com/anthonynanfito/case_study_cyclistic",
         img: "./media/case-study_cyclista_screenshot.png",
         alt: "Screen capture of 'CASE STUDY: How does a bike share navigate speedy success?'",
@@ -8,84 +8,77 @@ export const projectsArray = [
     },
     {
         id: "mathemaquizzo",
-        href: "https://github.com/ananfito/mathemaquizzo",
+        href: "https://ananfito.github.io/mathemaquizzo/",
         img: "./media/mathematquizzo.png",
         alt: "webpage with the title 'MathemaQuizzo' and instructions for how to start the quiz game",
         title: "MathemaQuizzo"
     },
     {
         id: "learning-journal",
-        href: "https://github.com/ananfito/learning-journal",
+        href: "https://ananfito.github.io/learning-journal/",
         img: "./media/screenshot_desktop_learning-journal-index.png",
         alt: "the desktop version of blog website with a navbar, hero image, and grid of blog posts",
         title: "Learning Journal (Blog)"
     },
     {
         id: "restaurant-ordering-app",
-        href: "https://github.com/ananfito/restaurant-ordering-app",
+        href: "https://ananfito.github.io/restaurant-ordering-app/",
         img: "./media/screenshot_desktop_restaurant-ordering-app.png",
         alt: "a webpage for a fictional plant-based restaurant called Tony's Bistro featuring a hero image at the top with a plate of burgers and three menu items: pizza, hamburger, beer",
         title: "Restaurant Ordering App"
     },
     {
         id: "to-do-list",
-        href: "https://github.com/ananfito/to-do-list",
+        href: "https://ananfito.github.io/to-do-list/",
         img: "./media/to-do-list-screenshot.png",
         alt: "input textbox centered on a page with a plus sign for adding to the list set with background of a laptop, glasses, and journal",
         title: "To-Do List"
     },
     {
-        id: "tetris-app-link",
-        href: "https://github.com/ananfito/basic-tetris",
+        id: "tetris-app",
+        href: "https://ananfito.github.io/basic-tetris/",
         img: "./media/basic-tetris-app.png",
         alt: "tetris app centered on the page with blocks falling and instructions printed at the top",
         title: "Tetris App"
     },
     {
-        id: "password-generator-link",
-        href: "https://github.com/ananfito/password-generator",
+        id: "password-generator",
+        href: "https://ananfito.github.io/password-generator/",
         img: "./media/password-generator.png",
         alt: "a random password generator centered on a page with a dark theme and button to generator a 15 character password",
         title: "Password Generator"
     },
     {
-        id: "basketball-scoreboard-link",
-        href: "https://github.com/ananfito/basketball-scoreboard",
+        id: "basketball-scoreboard",
+        href: "https://ananfito.github.io/basketball-scoreboard/",
         img: "./media/basketball-scoreboard.png",
         alt: "basketball scoreboard with home and vistor scores, and buttons for plus 1, plus 2, plus 3, minus 1, and reset",
         title: "Basketball Scoreboard"
     },
     {
-        id: "quote-generator-link",
-        href: "https://github.com/ananfito/random-quote-generator",
+        id: "random-quote-generator",
+        href: "https://ananfito.github.io/random-quote-generator/",
         img: "./media/random-quote-generator.png",
         alt: "a famous quote centered on the page with a generate button to start a new quote",
         title: "Random Quote Generator"
     },
     {
-        id: "stopwatch-demo-link",
-        href: "https://github.com/ananfito/stopwatch",
-        img: "./media/stopwatch-screenshot.png",
-        alt: "digital stopwatch with a start and reset button with a background of an hourglass resting on pebbles",
-        title: "Stopwatch"
-    },
-    {
         id: "passenger-counter-app",
-        href: "https://github.com/ananfito/passenger-counter-app",
+        href: "https://ananfito.github.io/passenger-counter-app/",
         img: "./media/px-app.png",
         alt: "passenger counter app with 3 buttons; increment, decrease, and reset",
         title: "Passenger Counter App"
     },
     {
-        id: "survey-form-link",
-        href: "https://github.com/ananfito/survey-form",
+        id: "survey-form",
+        href: "https://ananfito.github.io/survey-form/",
         img: "./media/screenshot_survey-form.png",
         alt: "survey form for a podcast asking for listener feedback on how they like the episodes",
         title: "Survey Form"
     },
     {
         id: "registation-form-link",
-        href: "https://github.com/ananfito/reg-form",
+        href: "https://ananfito.github.io/reg-form/",
         img: "./media/screenshot_reg-form.png",
         alt: "a basic registration form for a website with inputs for name, email, account type, profile image, and bio",
         title: "Registration Form"
